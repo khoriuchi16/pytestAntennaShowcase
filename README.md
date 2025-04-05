79e8f4d8bb12977391a140642e0fb468a7703c93
khoriuchi16 <kylehoriuchi@outlook.com>
2025-04-05 01:04:36 -0700

    added the firmware manager, log manager, and beam steering system with corresponding test cases for all functional requirements

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ pip install -r requirements.txt
 
 ## To run test suite
 pytest -s
+
+
+## Ways to induce failure in test_antenna.py
+Line 20: connection_interface.write(b'OTA_AUTH admin pass123\n') => change the username/password to something incorrect
+
+Line 92: time.sleep(2) => induces failure if > 3 seconds
+
+Line 118: current_position = current_position + 0.5 => some pertubuation or error; induces failure if > 1 degree
--- pseudo_hardware.py
+++ pseudo_hardware.py
@@ -6,6 +6,7 @@ import time
 import io
 import random
         
+
 class AuthenticationManager:
     def __init__(self):
         self.authenticated = False
@@ -13,11 +14,69 @@ class AuthenticationManager:
     def login(self, username, password):
         if username == "admin" and password == "pass123":
             self.authenticated = True
+            print("AUTHENTICATION SUCCESS")
             return "AUTHENTICATION SUCCESS"
         else:
             return "AUTHENTICATION FAIL"
         
 
+class FirmwareManager:
+    def __init__(self):
+        self.current_version = "v1.0"
+        self.last_good_version = "v1.0"
+        self.valid_firmware_uploaded = False
+        self.failed_validation = False
+
+    def upload(self, firmware_name):
+        if "corrupted" in firmware_name:
+            self.failed_validation = True
+            print("UPLOAD FAILED: CORRUPTED FILE")
+            return "UPLOAD FAILED: CORRUPTED FILE"
+        self.valid_firmware_uploaded = True
+        print("UPLOAD ACCEPTED")
+        return "UPLOAD ACCEPTED"
+    
+    def apply(self):
+        if self.failed_validation:
+            self.current_version = self.last_good_version
+            print("ROLLBACK TO v1.0")
+            return "ROLLBACK TO v1.0"
+        self.current_version = "v2.0"
+        print("FIRMWARE UPDATED SUCCESSFULLY")
+        return "FIRMWARE UPDATED SUCCESSFULLY"
+    
+    def get_version(self):
+        return self.current_version
+
+
+class LogManager:
+    def __init__(self):
+        self.logs = ["[0s] BOOT"]
+        self.events = []
+    
+    def retrieve(self):
+        print("\n".join(self.logs))
+        print("LOGS RECEIVED")
+        return "\n".join(self.logs)
+    
+    def get_status(self):
+        print("STATUS: ALL SYSTEMS NORMAL")
+        return "STATUS: ALL SYSTEMS NORMAL"
+    
+
+class BeamSteeringSystem:
+    def __init__(self):
+        self.theta = 0.0
+
+    def set_beam(self, theta):
+        self.theta = float(theta)
+        print(f"BEAM SET TO {theta} degrees")
+        return f"BEAM SET TO {theta}"
+    
+    def get_beam(self):
+        #print(f"BEAM AT: {self.theta} degrees")
+        return f"BEAM AT: {self.theta}"
+    
 
 #Mock serial class for creating hardware interface
 class PseudoSerial:
@@ -27,6 +86,10 @@ class PseudoSerial:
         self.last_command = ""
 
         self.auth = AuthenticationManager() #incorporate authentication manager test system
+        self.firmware = FirmwareManager() #incorporate firmware manager test system
+        self.logs = LogManager() #incorporate logging manager test system
+        self.beam = BeamSteeringSystem() #incorporate beam steering test system
+
 
         #simulate bad connection (50% chance of bad connection)
         connectionChance = random.randint(0,1)
@@ -55,6 +118,26 @@ class PseudoSerial:
     #For Authentication Manager
         if command.startswith("OTA_AUTH"):
             response = self.auth.login(parts[1], parts[2])
+    
+    #For Firmware Manager
+        elif command.startswith("OTA_UPLOAD"):
+            response = self.firmware.upload(parts[-1])
+        elif command.startswith("OTA_APPLY"):
+            response = self.firmware.apply()
+        elif command.startswith("FIRMWARE_VERSION"):
+            response = self.firmware.get_version()
+
+    #For Logging Manager
+        elif command.startswith("GET_LOG"):
+            response = self.logs.retrieve()
+        elif command.startswith("STATUS_CHECK"):
+            response = self.logs.get_status()
+
+    #For Beam Steering System
+        elif command.startswith("SET_BEAM"):
+            response = self.beam.set_beam(parts[1])
+        elif command.startswith("GET_BEAM"):
+            response = self.beam.get_beam()
 
 
         self.buffer = io.StringIO(response + "\n")
